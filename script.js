eff8cb49444286a3b9702ec198dfa2c47ebc3402
Tuan Hoang <nguyenledly1997@gmail.com>
2022-09-11 21:08:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var end = moment("2023-02-28");
+
+function getLeftTime() {
+    var now = moment();
+    var diffÍnSecond = end.diff(now, "second");
+    return {
+        "days": Math.floor(diffÍnSecond / 3600 / 24),
+        "hours": Math.floor(diffÍnSecond / 3600) % 24,
+        "minutes": Math.floor(diffÍnSecond / 60) % 60,
+        "seconds": Math.floor(diffÍnSecond % 60),
+    }
+}
+
+var timeDiv = document.getElementsByClassName("timer-display");
+var mainContainer = document.getElementById("main_container");
+mainContainer.classList.add('rand-bg-' + (Math.floor(Math.random() * 4 + 1)))
+
+setInterval(function () {
+    var time = getLeftTime();
+    timeDiv.days.innerHTML = time.days;
+    timeDiv.hours.innerHTML = time.hours;
+    timeDiv.minutes.innerHTML = time.minutes;
+    timeDiv.seconds.innerHTML = time.seconds;
+}, 1000);
